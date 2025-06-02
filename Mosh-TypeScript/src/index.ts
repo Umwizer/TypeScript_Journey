98467b3e70f3964c19450440f02f1a28d366b40d
Umwizer <ruthumwizerwa@gmail.com>
2025-06-02 11:14:55 +0300

    real use of TypeScript Intro

--- Mosh-TypeScript/src/index.ts
+++ Mosh-TypeScript/src/index.ts
@@ -1,2 +1,9 @@
-let age: number = 20;
-if (age < 50) age += 10;
+let sales = 123_456_789; //in ts if you have large value of
+//number you can separate them using underscore to make them more readable.
+//because we assigned the value to number automatically Js make t number
+let course: string = "TypeScript";
+let is_published = true;
+let level; //ts assumes this available can take any value;
+// level = 1;
+// level = "a"; // as see it taked different values
+//but as you know this will be against the use of including typescript
