ae88da5bebc6dac24d1fcc1855aac3daeb5c2c36
Umwizer <ruthumwizerwa@gmail.com>
2025-06-02 10:33:20 +0300

    creating object and adding values in ts

---
+++ Scrimba/index.js
@@ -0,0 +1,12 @@
+var properties1 = [
+  {
+    image: "https://example.com/property1.jpg",
+    title: "Seaside Villa",
+    price_pernight: 120,
+    "town/city": "Malibu",
+    "postcode/zip-code": 90265,
+    country: "USA",
+    contactdetails: 1234567890,
+    availabletorent: true,
+  },
+];
---
+++ Scrimba/index.ts
@@ -0,0 +1,21 @@
+const properties: {
+  image: string;
+  title: string;
+  price_pernight: number;
+  "town/city": string;
+  "postcode/zip-code": number;
+  country: string;
+  contactdetails: number;
+  availabletorent: boolean;
+}[] = [
+  {
+    image: "https://example.com/property1.jpg",
+    title: "Seaside Villa",
+    price_pernight: 120,
+    "town/city": "Malibu",
+    "postcode/zip-code": 90265,
+    country: "USA",
+    contactdetails: 1234567890,
+    availabletorent: true,
+  },
+];
