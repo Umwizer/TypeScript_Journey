1bd03c0f2c8196a9b3714f4e46ad60af8aed13ea
Umwizer <ruthumwizerwa@gmail.com>
2025-06-04 13:22:10 +0200

    nullishcoaelscing

---
+++ Mosh-TypeScript/src/advanced/NullishCoaelscing.ts
@@ -0,0 +1,5 @@
+let speed: number | null = null;
+let ride = {
+  //falsy (undefined,null,'',false,0)
+  speed: speed ?? 30,
+};
--- Mosh-TypeScript/src/advanced/optionalChaining.ts
+++ Mosh-TypeScript/src/advanced/optionalChaining.ts
@@ -6,4 +6,12 @@ function getCustomer(id: number): Customer | null | undefined {
 }
 let customer = getCustomer(1);
 //Optional propert access operator
-console.log(customer?.birthday);
+console.log(customer?.birthday?.getFullYear());
+
+//Optional element access operator
+//if(customers !== null && customers != undefined)
+//customers?.[0]
+//optional call
+//let log: any = (message: string) => console.log(message);
+let log: any = null;
+log?.("a");
