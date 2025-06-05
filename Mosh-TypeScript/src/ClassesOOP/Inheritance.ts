2a78436f09c5e3779e52855b236c7b6dc10cf87f
Umwizer <ruthumwizerwa@gmail.com>
2025-06-05 13:34:46 +0200

    inheritance

---
+++ Mosh-TypeScript/src/ClassesOOP/Inheritance.ts
@@ -0,0 +1,37 @@
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+  Walk() {
+    console.log("Walking");
+  }
+}
+class Student extends Person {
+  constructor(public studentId: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+  takeTest() {
+    console.log("Taking Test");
+  }
+}
+// let student = new Student(1, "John", "john@gmail.com");
+// student.takeTest;
+// Getters (get) and methods like Walk() should not be defined inside the constructor. They should be class-level methods, outside the constructor
+
+class Teacher extends Person {
+  override get fullName() {
+    return "Professor " + super.fullName;
+  }
+}
+let teacher = new Teacher("Ruth", "Umwizerwa");
+console.log(teacher.fullName);
+
+//polymorphism
+printNames([
+  new Student(1, "Kellia", "Iradukunda"),
+  new Teacher("Ruth", "Umwizerwa"),
+]);
+function printNames(people: Person[]) {
+  for (let person of people) console.log(person.fullName);
+}
--- Mosh-TypeScript/src/ClassesOOP/StaticMeb.ts
+++ Mosh-TypeScript/src/ClassesOOP/StaticMeb.ts
@@ -2,4 +2,22 @@ class Ride {
   //passenger
   //pickUpLocation
   //DropoffLocation
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+  static get activeRides() {
+    return Ride._activeRides;
+  }
 }
+// Ride.activeRides = 10;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
