6544c15862a1a92ca53bbdae399aba9ed3f618eb
Umwizer <ruthumwizerwa@gmail.com>
2025-06-05 10:18:50 +0200

    typeassertion

---
+++ Mosh-TypeScript/src/advanced/typeAssertions.ts
@@ -0,0 +1,6 @@
+let phone = document.getElementById("phone") as HTMLInputElement;
+let phone1 = <HTMLInputElement>document.getElementById("phone");
+//HTMLElement
+//HTMLInputElement
+
+phone.value;
