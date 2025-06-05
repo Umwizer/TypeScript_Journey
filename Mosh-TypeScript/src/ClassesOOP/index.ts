b7dba3a7fa0d621ff780aa52d6f4804eba7463c9
Umwizer <ruthumwizerwa@gmail.com>
2025-06-05 11:43:35 +0200

    parameter properties

--- Mosh-TypeScript/src/ClassesOOP/index.ts
+++ Mosh-TypeScript/src/ClassesOOP/index.ts
@@ -1,17 +1,21 @@
 class Account {
-  readonly id: number; //readonly
-  owner: string;
-  balance: number;
+  // readonly id: number; //readonly
+  // owner: string;
+  //balance: number;
   nickName?: string; //readonly properties
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number //Parameter Properties
+  ) {
+    // this.id = id;
+    //this.owner = owner;
+    //this.balance = balance;
   }
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
-    this.balance += amount;
+    this._balance += amount;
   }
 }
 
