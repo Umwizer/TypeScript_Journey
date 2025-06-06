b2b5f89de0ea3ab00b71680cd23d3c695954437f
Umwizer <ruthumwizerwa@gmail.com>
2025-06-06 17:56:29 +0200

    understanding the problem

---
+++ Mosh-TypeScript/src/Generics/index.ts
@@ -0,0 +1,9 @@
+class keyValuePair {
+  constructor(public key: number, public value: string) {}
+}
+
+class StringKeyValuePair {
+  constructor(public key: string, public value: string) {}
+}
+let pair = new StringKeyValuePair("1", "Apple");
+pair.key;
