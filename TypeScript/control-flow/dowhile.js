52edc153248cf903dcc43ef48990c29b0ce75eb6
Umwizer <ruthumwizerwa@gmail.com>
2025-06-12 12:23:35 +0200

    dowhile

---
+++ TypeScript/control-flow/dowhile.js
@@ -0,0 +1,5 @@
+var num = 0;
+do {
+    console.log("Number is ".concat(num));
+    num++;
+} while (num < 3);
---
+++ TypeScript/control-flow/dowhile.ts
@@ -0,0 +1,6 @@
+let num: number = 0;
+
+do {
+  console.log(`Number is ${num}`);
+  num++;
+} while (num < 3);
