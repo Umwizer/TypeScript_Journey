c8bf73668de845574dfaf34d90511b95dc42d11b
Umwizer <ruthumwizerwa@gmail.com>
2025-06-17 11:09:12 +0200

    dec

--- Mosh-TypeScript/src/decorators/index.ts
+++ Mosh-TypeScript/src/decorators/index.ts
@@ -1,9 +1,13 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called ");
-  constructor.prototype.UniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the Dom");
+//Decorator factory
+function Component(value: number) {
+  return (constructor: Function) => {
+    console.log("Component decorator called ");
+    constructor.prototype.options = value;
+    constructor.prototype.UniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the Dom");
+    };
   };
 }
-@Component
+@Component(1)
 class ProfileComponent {}
