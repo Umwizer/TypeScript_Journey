4b0939ec2e415be5f7b490f6f93766c3e7fa553c
Umwizer <ruthumwizerwa@gmail.com>
2025-06-17 10:23:41 +0200

    decorators

--- Mosh-TypeScript/src/decorators/index.ts
+++ Mosh-TypeScript/src/decorators/index.ts
@@ -1,5 +1,5 @@
 function Component(constructor: Function) {
-  console.log("Componentdecorator called ");
+  console.log("Component decorator called ");
   constructor.prototype.UniqueId = Date.now();
   constructor.prototype.insertInDOM = () => {
     console.log("Inserting the component in the Dom");
